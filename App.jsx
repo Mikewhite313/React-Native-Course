1addf591bfd93156a5dacbd097529722c0b96daa
Mikewhite@313 <syedanasahmed15@gmail.com>
2024-11-07 17:41:32 -0800

    view, Button, Style

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Button, ImageBackground } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style = {{fontSize: 50 }}>Hello World</Text>
+      <Button title='Press Me Here'/>
+    </View>
+  );
+};
+
+export default App;
